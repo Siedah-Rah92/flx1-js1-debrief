a663c0c914a9ab2bf663837c8c1afa7f91142480
Siedah-Rah92 <130437292+Siedah-Rah92@users.noreply.github.com>
2023-05-10 13:46:28 -0500

    I have made an edit to a file!

---
+++ script.js
@@ -0,0 +1,36 @@
+let myNumber = 15;
+
+if(typeof myNumber !== 'number') {
+    console.log('This is not a number')
+} else if(myNumber % 3 === 0) {
+    console.log('Gen')
+} else if(myNumber % 5 === 0) {
+    console.log('Buzz')
+} else if(myNumber % 3 === 0 && myNumber % 5 === 0) {
+    console.log('GenBuzz')
+} else {
+    console.log(myNumber)
+}
+
+
+let item = 'Skirts';
+switch(item) {
+    case 'Shoes':
+    console.log('Shoes are $50');
+    break;
+    case 'Jeans':
+    console.log('Jeans are $25');
+    break;
+    case 'Hats':
+    console.log('Hats are $12');
+    break;
+    case 'Socks':
+    console.log('Socks are $2');
+    break;
+    default:
+    console.log('Invalid Item');
+    break;
+}
+
+
+console.log(Math.floor(Math.random() * 51 +50))
